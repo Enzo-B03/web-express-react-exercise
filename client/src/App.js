c189cfbfd69dbe2e54a3784784255382a2142510
Virgilio JR T. FLORES <v.t.flores@smmph.local>
2021-02-18 15:19:40 +0800

    edit to try and get questions from an api

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,37 @@ class App extends Component{
     axios.get('/api/v1/say-something').then((res) => {
       const response = res.data;
       this.setState({response});
+      console.log("from api " + res.data[0].question);
+      response.map(item=>{
+        console.log(item[0].question)
+      });
     });
   };
-  render() {
+
+
+  // renderAnswerOptions(key) {
+  //   return (
+  //     <AnswerOption
+  //       key={key.content}
+  //       answerContent={key.content}
+  //       answerType={key.type}
+  //       answer={props.answer}
+  //       questionId={props.questionId}
+  //       onAnswerSelected={props.onAnswerSelected}
+  //     />
+  //   );
+  // }
+
+    render() {
     return (
       <div className="App">
         <h1>Hello from the front-end</h1>
-        <h1>{this.state.response.body}</h1>
+        <h1>{this.state.response}</h1>
       </div>
     )
   }
 };
 
 export default App;
+
+
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,97 @@
 const saySomething = (req, res, next) => {
-    res.status(200).json({
-        body: "hello from server!"
-    });
+    res.status(200).json(
+         [
+            {
+              question: "What is the meaning of the acronym EMS?",
+              answers: [
+                  {
+                      type: "correct",
+                      content: "Environment Management System"
+                  },
+                  {
+                      type: "wrong1",
+                      content: "Every Man's Suffering"
+                  },
+                  {
+                      type: "wrong2",
+                      content: "Established Management Style"
+                  },
+                  {
+                    type: "wrong3",
+                    content: "Engineering Management System"
+                }
+              ]
+            },
+            {
+                question: "What is the meaning of the acronym SWM?",
+                answers: [
+                    {
+                        type: "correct",
+                        content: "Solid Waste Management"
+                    },
+                    {
+                        type: "wrong1",
+                        content: "Swim With Marlins"
+                    },
+                    {
+                        type: "wrong2",
+                        content: "So What M@#$%$*!"
+                    }
+                ]
+            },
+            {
+                question: "How many H2S leak incidents have we encountered?",
+                answers: [
+                    {
+                        type: "correct",
+                        content: "One"
+                    },
+                    {
+                        type: "wrong1",
+                        content: "Two"
+                    },
+                    {
+                        type: "wrong2",
+                        content: "Three"
+                    }
+                ]
+            },
+            {
+                question: "When will the imposed employee isolation be lifted?",
+                answers: [
+                    {
+                        type: "correct",
+                        content: "Next Month"
+                    },
+                    {
+                        type: "wrong1",
+                        content: "Next Year"
+                    },
+                    {
+                        type: "wrong2",
+                        content: "Never"
+                    }
+                ]
+            },
+            {
+                question: "What does the acronym DCS mean?",
+                answers: [
+                    {
+                        type: "correct",
+                        content: "Distributed Control System"
+                    },
+                    {
+                        type: "wrong1",
+                        content: "Dangerous Control System"
+                    },
+                    {
+                        type: "wrong2",
+                        content: "Deadly Control Strategy"
+                    }
+                ]
+            }
+          ]
+)
 };
-
 module.exports.saySomething = saySomething;
+
